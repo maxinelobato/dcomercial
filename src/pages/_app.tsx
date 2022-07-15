e8353c0a9159865cce7987a7f7df34cb4af5a837
maxinelobato <maxinelobato89@hotmail.com>
2022-07-15 02:55:58 -0300

    change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import theme from "./theme";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
