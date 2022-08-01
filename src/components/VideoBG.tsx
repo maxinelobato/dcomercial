71da8fc957dd938f6f897e1acb3948b2936a9c97
maxinelobato <maxinelobato89@hotmail.com>
2022-08-01 20:41:05 -0300

    chaangevideo

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -5,7 +5,9 @@ export function VideoBG() {
   return (
     <VideoPlayer
       className={styles.reactvideocover}
-      src="/src/components/video/index.mp4"
+      src={
+        "https://player.vimeo.com/external/435674703.sd.mp4?s=01ad1ba21dc72c1d34728e1b77983805b34daad7&profile_id=165&oauth2_token_id=57447761"
+      }
       autoPlay="true"
       unmute="true"
       preload="auto"
