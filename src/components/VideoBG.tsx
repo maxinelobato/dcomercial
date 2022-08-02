64b24d0bb559651f2702ef77952aeb3027ac3349
lvneto <lauro.neto.1995@gmail.com>
2022-08-02 17:54:18 -0300

    testing

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -1,16 +1,11 @@
 import VideoPlayer from "react-background-video-player";
 import styles from "./video/styles.module.css";
+import sample from './video/index.mp4';
 
 export function VideoBG() {
   return (
-    <VideoPlayer
-      className={styles.reactvideocover}
-      src={
-        "https://player.vimeo.com/external/435674703.sd.mp4?s=01ad1ba21dc72c1d34728e1b77983805b34daad7&profile_id=165&oauth2_token_id=57447761"
-      }
-      autoPlay="true"
-      unmute="true"
-      preload="auto"
-    />
+    <video className='videoTag' autoPlay loop muted>
+      <source src={sample} type='video/mp4' />
+    </video>
   );
 }
