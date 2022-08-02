5361653eaff42c3d83333d02e41d9a7d6d302761
maxinelobato <maxinelobato89@hotmail.com>
2022-08-02 18:03:28 -0300

    changesvideobg

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -1,11 +1,11 @@
 import VideoPlayer from "react-background-video-player";
 import styles from "./video/styles.module.css";
-import sample from './video/index.mp4';
+import sample from "./video/index.mp4";
 
 export function VideoBG() {
   return (
-    <video className='videoTag' autoPlay loop muted>
-      <source src={sample} type='video/mp4' />
+    <video className={styles.reactvideocover} autoPlay loop muted>
+      <source src={sample} type="video/mp4" />
     </video>
   );
 }
