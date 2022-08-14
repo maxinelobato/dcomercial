8849c71d2886a5fed3aad6740c4fee54e55ce7ac
maxinelobato <maxinelobato89@hotmail.com>
2022-08-14 14:35:46 -0300

    changesall

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="vite/client" />
 declare module "react-reveal/Jump";
 declare module "react-typed";
-declare module "vite-plugin-radar";
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteRadar from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteRadar({
-      enableDev: true,
-      gtm: {
-        id: "GTM-PMX3LP3",
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
   },
