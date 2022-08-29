cf9750dbbeb038bdccc65a1e0b3ecd4017b08da1
maxinelobato <maxinelobato89@hotmail.com>
2022-08-29 12:11:17 -0300

    themets

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  extendTheme,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import ButtonCTAWhats from "./components/ButtonCTA/ButtonCTAWhats";
 
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider resetCSS>
+      <ChakraProvider resetCSS theme={theme}>
         <ButtonCTAWhats />
         <App />
       </ChakraProvider>
